a5474a40cd962d1d910bb18f0736c89508a22db0
Hikaru Yorioka <hyorioka@inet.jeis.co.jp>
2024-08-09 18:33:36 +0900

    add frontend app

---
+++ backend/pear-art/src/main/java/com/pear/hyorioka/pear_art/PearArtApplication.java
@@ -0,0 +1,14 @@
+package com.pear.hyorioka.pear_art;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PearArtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PearArtApplication.class, args);
+		System.out.println("Hello, Pear Art!");
+	}
+
+}
